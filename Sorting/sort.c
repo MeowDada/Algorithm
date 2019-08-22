f7d21e10d3cb928882cdf0bf72c5c6b783e1ec8a
MeowDada <chang.hungjui1251@gmail.com>
2019-08-22 13:05:01 +0800

    Add selection sort

--- Sorting/sort.c
+++ Sorting/sort.c
@@ -4,6 +4,7 @@
 
 const static void (*_sort_method_table[])(void *, size_t, size_t, int(*)(const void *, const void *)) = {
     bubble_sort,
+    selection_sort,
 };
 static void (*_sort_method)(void *, size_t, size_t, int(*)(const void *, const void *)) = NULL;
 
